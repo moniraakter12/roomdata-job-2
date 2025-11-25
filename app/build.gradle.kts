636bfd4c92f152e00b12a08a02497fef05a6cd20
Monira <monira.amad-06-13@dipti.com.bd>
2025-11-25 12:00:56 +0600

    2nd commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,20 +43,23 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // DO NOT USE this → libs.androidx.activity (it may include activity 1.11.0)
+    // implementation(libs.androidx.activity)
+
+    // ✔ Correct Activity version for AGP 8.2.2
+    implementation("androidx.activity:activity:1.9.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // ViewModel & LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    dependencies {
-        // Room Database
-        implementation ("androidx.room:room-runtime:2.6.1")
-        implementation ("androidx.room:room-ktx:2.6.1")
-        ksp ("androidx.room:room-compiler:2.6.1")
-
-
-        // ViewModel and LiveData
-        implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-        implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    }
-
 }
